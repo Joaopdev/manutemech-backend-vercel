3279816b00d1ab1988c91ef171f7069a385523ae
JoaoP <joaotst@hotmail.com>
2023-07-17 14:42:57 -0300

    postgresql config

--- prisma/migrations/20230524171426_bug_fix/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `address` to the `Supplier` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `supplier` ADD COLUMN `address` VARCHAR(191) NOT NULL;
